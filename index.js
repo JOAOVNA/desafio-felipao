51c14b4252c4ed4ff10785ebb48ec88f0411a80b
JOAOVNA <vitornavarro20@gmail.com>
2024-08-02 16:20:44 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-let desafio = "Desafio do felipão"
+let heroi = "Brendon"
+let xp = 23000
+let elo = ""
 
-console.log (desafio)
+if (xp < 1000) {
+    elo = "Ferro"
+}else if ((xp >= 1001) && (xp <= 2000)){
+    elo = "Bronze"
+}else if ((xp >= 1001) && (xp <= 2000)){
+    elo = "Prata"
+}else if ((xp >= 2001) && (xp <= 5000)){
+    elo = "Ouro"
+}else if ((xp >= 5001) && (xp <= 7000)){
+    elo = "Platina"
+}else if ((xp >= 7001) && (xp <= 8000)){
+    elo = "Ascendente"
+}else if ((xp >= 8001) && (xp <= 9000)){
+    elo = "Imortal"
+}else if(xp >= 10001){
+    elo = "Radiante"
+}
+
+console.log("O Herói de nome " + heroi + " está no nível " + elo)
+
+
